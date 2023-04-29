bff38cb36691a58b74e3e884d4c924953e06974c
Trader <doro.kon@mail.ru>
2023-04-29 11:34:04 +0300

    Initial commit

---
+++ src/main/java/ru/trader/dental_clinik/DentalClinikApplication.java
@@ -0,0 +1,13 @@
+package ru.trader.dental_clinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalClinikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/trader/dental_clinik/DentalClinikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trader.dental_clinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
